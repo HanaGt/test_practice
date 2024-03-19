26a902572a1bee09024d8de8c50bde34041a32c8
HanaGt <hana.guta-ug@aau.edu.et>
2024-03-19 11:24:34 +0300

    update product and shopping cart

---
+++ product.py
@@ -0,0 +1,11 @@
+import unittest
+
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+    def calculateTotal(self):
+        return self.price * self.quantity
+
